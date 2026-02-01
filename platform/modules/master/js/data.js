751b8cec3f3365433defaba9ac4e28172f5cd611
PauloParreira\Paulo H Parreira <parreirapaulohenrique@gmail.com>
2026-02-01 00:00:31 -0300

    deploy: AtualizaÃ§Ã£o automÃ¡tica - 2026-02-01 00:00

--- platform/modules/master/js/data.js
+++ platform/modules/master/js/data.js
@@ -1,15 +1,15 @@
 export const mockTenants = [
     {
-        id: 't_parreira',
-        name: 'Parreira Logística (Matriz)',
-        cnpj: '37.123.456/0001-99',
-        adminEmail: 'paulo@parreiralog.com.br',
+        id: 'ltdistribuidora',
+        name: 'LT Distribuidora Peças Motos',
+        cnpj: '08.747.452/0001-43',
+        adminEmail: 'paulo@ltdistribuidora.com.br',
         modules: ['dispatch', 'master', 'wms'],
         status: 'active',
         createdAt: '2024-01-01'
     },
     {
-        id: 't_central',
+        id: 'centralpecas',
         name: 'Central Peças',
         cnpj: '12.987.654/0001-11',
         adminEmail: 'admin@centralpecas.com.br',
